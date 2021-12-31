a28170468dd9bd1d6a524b9162849e5c8e18f326
robonexx <robertwagar@gmail.com>
2021-12-31 14:55:46 +0100

    redirecting paths by using navigate in react-router-dom 6. added a redirect back to homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -21,6 +21,7 @@ function App() {
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/articles/:id' element={<Article />} />
+          <Route path='*' element={<Navigate to='/' />} />
         </Routes>
       </BrowserRouter>
     </div>
